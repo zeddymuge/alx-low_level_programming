dbad576ffbb8abe0afb960e100996194f23ed619
zeddy cherotich <cheroozeddy@gmail.com>
2022-06-30 08:03:57 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main- entry point
+ * Description: 'C pogram for print
+ * Return: Always 0 (sucess)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
