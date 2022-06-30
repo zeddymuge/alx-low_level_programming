850d12d0b46e0e3c2e0babaf5d7ccaafc1332aab
zeddy cherotich <cheroozeddy@gmail.com>
2022-06-30 10:18:06 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-mEntry point
+ *Description: C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
