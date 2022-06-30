f3fd637d713630abaa88b10b5958285c53c17b2e
zeddy cherotich <cheroozeddy@gmail.com>
2022-06-30 08:24:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main -Entry poin
- * descriptin: 'put'
- * retutn : 0
- */
+ * main -Entry point
+ * Description: 'c program for puts'
+ *Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
 }
