c4fbcd08b7c898d72dabe9a9492a4e96ea6fcd1d
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 06:35:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 0)
+		printf("%d/n", x);
+	x + 1;
+
+	return (0);
+}
