39746d6d4bcfc5ec427857a7d577268661005b65
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 06:56:23 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
