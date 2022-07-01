fe5a23a18a6a749aaa23c12b72db11c03167e956
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 07:51:02 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9', x++)
+		putchar(x);
+	if (x != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
