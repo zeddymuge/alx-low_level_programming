5c8226d525abcbef702ab9a09035e138181562c5
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 05:34:33 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	putchar(n);
+	putchar('\n');
+
+	return (0);
+}
