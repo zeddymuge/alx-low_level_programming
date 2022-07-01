3ec86f8accce2ae3df76505b3be4813629ae023b
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 05:12:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("last digit of %d is\n", n)
+	printf("last digit of %d is\n", n);
 
 	if (n > 5)
 		printf(" greater than 5");
