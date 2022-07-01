88ac06301fe6c6d9132b3ac25675fdbf4c7344d5
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 12:35:41 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '98'; x++)
+	{
+		for (y = x + 1; y <= '99'; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 98 && y == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+
+	return (0);
+}
