e8e56b44309dc11af9c4d8e9276afd60246474cc
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 12:38:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int x, y;
 
-	for (x = '0'; x <= '98'; x++)
+	for (x = '0'; x <= 98; x++)
 	{
-		for (y = x + 1; y <= '99'; y++)
+		for (y = x + 1; y <= 99; y++)
 		{
 			putchar((x / 10) + '0');
 			putchar((x % 10) + '0');
