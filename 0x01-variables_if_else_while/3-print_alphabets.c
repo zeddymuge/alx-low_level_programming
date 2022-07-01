cbc40913813f0cddd49e66f09a6e45bead1024ff
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 06:01:45 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
