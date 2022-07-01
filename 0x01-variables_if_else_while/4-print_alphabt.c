78d7d605329f86b59d930bc1c017e0770ed05575
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 06:17:59 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
