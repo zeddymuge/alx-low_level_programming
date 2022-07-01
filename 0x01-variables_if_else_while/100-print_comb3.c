5b5aeb51d2c84cd28eca28ea6cf4c6aaca86a5c0
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-01 08:38:38 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x < '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			if (x != y)
+			{
+				putchar(x);
+				putchar(y);
+
+				if (x == '8' && y == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
