c2f67ba9dc53d6e8ea9b562a84f5dae884855a98
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-05 03:47:49 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar("i");
+	_putchar('\n');
+
+	return (0);
+}
