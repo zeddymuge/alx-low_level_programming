080d11e35a55c2848e0d6345597f8a3eaceb4a13
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-05 04:00:02 -0700

    h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 
@@ -12,5 +12,4 @@ int main(void)
 		_putchar(i);
 	_putchar('\n');
 
-	return (0);
 }
