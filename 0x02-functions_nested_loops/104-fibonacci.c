d76d5403b22bcfad6f18de6e9fcb7d9f1abec35c
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-05 13:03:17 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 /**
  * main - Entry point
  *
@@ -7,28 +8,30 @@
 int main(void)
 {
 	int i;
-	int first_no = 0;
-	int second_no = 1;
-	int next_no;
+	unsigned long int z1 = 0, y1 = 1, z2 = 0, y2 = 2;
+	unsigned long int a, b, c;
 
-	for (i = 0 ; i < 98 ; i++)
+	printf("%lu, %lu, ", y1, y2);
+	for (count = 2; count < 98; count++)
 	{
-		if (i <= 1)
-			next_no = i;
+		if (y1 + y2 > LARGEST || z2 > 0 || z1 > 0)
+		{
+			a = (y1 + y2) / LARGEST;
+			b = (y1 + y2) % LARGEST;
+			c = z1 + z2 + a;
+			z1 = z2, z2 = c;
+			y1 = y2, y2 = b;
+			printf("%lu%010lu", z2, y2);
+		}
 		else
 		{
-			next_no = first_no + second_no;
-			first_no = second_no;
-			second_no = next_no;
-
-			if (next_no > 98)
-			{
-				break;
-			}
+			b = y1 + y2;
+			y1 = y2, y2 = b;
+			printf("%lu", y2);
 		}
-		 printf("%d  ", next_no);
+		if (count != 97)
+			printf(", ");
 	}
-
+	printf("\n");
 	return (0);
 }
-
