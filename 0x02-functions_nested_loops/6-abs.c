cb32c96a7ecddc91625b5692614dd78b094ca447
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-05 07:33:10 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,14 +5,10 @@
  * Description: This function returns absolute value of a number
  * Return: Absolut value of number r
  */
-int _abs(int r);
+int _abs(int r)
 {
 	if (r >= 0)
-	{
 		return (r);
-	}
 	else
-	{
 		return (r * -1);
-	}
 }
