061d51a7d4be5f50dfe49cdc8891646960be0c5f
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-05 08:40:04 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * 10-add - Check description
+ * Check description: adds two integers
+ * Return: addition of x and y
+ */
+int add(int x, int y);
+{
+	int n;
+
+	n = x + y;
+	_putchar(n);
+
+	return (0);
+}
