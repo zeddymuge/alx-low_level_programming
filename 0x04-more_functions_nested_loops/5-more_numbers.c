78b838ace2e8d2d6d55ca740f0fbc58723847fd1
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-07 12:31:57 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,13 @@ void more_numbers(void)
 {
 	int a, b ;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (a > 9)
-				_putchar(a / 10 + '0');
-
-			_putchar(a % 10 + '0');
+			if (b > 9)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
