2a1167d2d7734663a724f34ad78d65733e17a216
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-11 06:57:51 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of a string
+ * @str: string parameter
+ */
+void puts_half(char *str)
+{
+	int x, y, z;
+
+	x = strlen(str);
+
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (z = y; z < x; z++)
+		_putchar(str[z]);
+	_putchar('\n');
+}
