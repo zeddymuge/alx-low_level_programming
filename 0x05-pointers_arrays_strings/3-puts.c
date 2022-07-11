1906e85ff7408a29dedf2f42d5ff0910801d8f1e
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-11 04:17:22 -0700

    3-puts.c;

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
- * _puts - function that prints a string
- * @str: input string
- * REturn: nothing
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @str: an input string
+ * Return: Nothing
  */
+
 void _puts(char *str)
 {
-	int i = 0;
-
-	for (; str[i] != '\0'; i++)
+	for(; *str != '\0'; str++)
 	{
-		_putchar(str[i]);
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
