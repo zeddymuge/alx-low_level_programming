a442ac8aadd6e75e7c3b26c24e19f0e8aca0cfdb
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-14 08:22:44 -0700

    all

--- 0-main.c
+++ 0-main.c
@@ -1,14 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i;
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
 
-	i = 0;
-	positive_or_negative(i);
-
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
 	return (0);
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 C programming using gcc compiler
-1. 0-main.c, main.h -this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project:
+1. 0-main.c, main.h -this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project
+:
+2. alx-low_level_programming - debugging repo
