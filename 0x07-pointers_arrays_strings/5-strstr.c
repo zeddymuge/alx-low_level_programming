948b4b5512215a8b004c94e79e95a6901aa14317
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-07-18 16:41:39 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,7 @@
  * @haystack: an input string to search in
  * @needle: an input string to locate into string haystack
  * Return:  a pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+ * OR NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
