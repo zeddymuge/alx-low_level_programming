a5258d6b21fe2f1136e3b6c1ca44d407ee260bf2
zeddy cherotich <cheroozeddy@gmail.com>
2022-07-22 14:44:47 -0700

    done

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,15 +1,9 @@
 #include "main.h"
 
-/**
- * main - checks main
- *
- * Return: Always 0.
- */
-
- int main(void)
- {
-	 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	 return (0);
- }
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
 
     
