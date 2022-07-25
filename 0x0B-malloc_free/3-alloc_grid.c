136ee230f8c5b1ccbf5a4df658aa3cfde15281b7
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-07-25 18:00:26 +0300

    everything

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,38 +1,49 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * alloc_grid - A function that returns a pointer to a
- * 2 dimensional array of integers
- * @width: An input integer at number of columns
- * @height: An input integer at number of rows
- * Return: pointer to a 2D array, NULL on failure
+ * argstostr - function that concatenates all the arguments of your program
+ *
+ * @ac: argument count
+ * @av: pointer to a string of pointers to strings
+ *
+ * Return: address of the newly allocated memory
  */
-int **alloc_grid(int width, int height)
+
+char *argstostr(int ac, char **av)
 {
-	int **array, x = 0, y = 0, z, n;
+	int x, y, z;
+	int len = 0;
+	char *arg_concat;
 
-	if (width <= 0 || height <= 0)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	array = (int **)malloc(sizeof(int *) * height);
-	if (array == NULL)
-		return (NULL);
-	for (; x < height; x++)
+	for (x = 0; x < ac; x++)
 	{
-		array[x] = (int *)malloc(sizeof(int) * width);
-		if (array[x] == NULL)
+		for (y = 0; av[x][y] != '\0'; y++)
+			len++;
+		len++;
+	}
+	len++;
+
+	arg_concat = malloc(len * sizeof(char));
+	if (arg_concat == NULL)
+	{
+		free(arg_concat);
+		return (NULL);
+	}
+
+	z = 0;
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++, z++)
 		{
-			for (; y < x; y++)
-			{
-				free(array[y]);
-			}
-			return (NULL);
+			arg_concat[z] = av[x][y];
 		}
+		arg_concat[z] = '\n';
+		z++;
 	}
-	for (z = 0; z < height; z++)
-	{
-		for (n = 0; n < width; n++)
-			array[z][n] = 0;
-	}
-	return (array);
+	return (arg_concat);
 }
