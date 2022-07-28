affacf354b3e9c02da22b23efd71a215f2c0cc41
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-07-28 15:44:23 +0300

    everything up

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,19 +9,27 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
-
+	int num1, num2;
+	unsigned long mul;
+	
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	else
+	for (num1 = 1; num1 < argc; num1++)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-		printf("%d\n", mul);
-		return (0);
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		{
+			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+
 	}
+	mul = atol(argv[1]) *atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
