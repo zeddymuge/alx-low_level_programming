636f4b8466416698bdf7ae65e7ccb62848dfb96d
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-07-28 15:48:49 +0300

    everythin up

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,26 +6,20 @@
  * @argc: n arguments
  * Return: int
  */
-
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	unsigned long mul;
-	
+unsigned long mul;
+int i, j;
 	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (num1 = 1; num1 < argc; num1++)
-	{
-		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
 		}
 
 	}
