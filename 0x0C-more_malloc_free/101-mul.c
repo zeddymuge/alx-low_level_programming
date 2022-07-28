bdeba23e06c34522827217c1d9605570c377ea66
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-07-28 17:32:00 +0300

    new test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,17 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <limits.h>
 /**
  * main - multiplies two positive numbers
  * @argc: n arguments
- * @argv: n arguments
+ * @argv: args
  * Return: int
  */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
-	
+unsigned long mul;
+int i, j;
 	if (argc != 3)
 	{ printf("Error\n");
 	exit(98); }
