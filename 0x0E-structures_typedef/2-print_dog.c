4ef8d13605ae859ded77728c63976e367947897d
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-01 13:10:09 +0300

    everything done !

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,12 +8,12 @@
  */
 void print_dog(struct dog *d)
 {
-
 	if (d == NULL)
-		printf("nil");
-	if (name == NULL)
-		printf("Name: nil");
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
