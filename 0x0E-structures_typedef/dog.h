1218b4496910009d7689f91e5685e05be3e19b5e
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-01 15:00:55 +0300

    everything done!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - holds varable
+ * @name: name
+ * @age: age of the user
+ * @owner: owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
