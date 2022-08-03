f098775cf6ce764a908de30a14b854703c5b1a52
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-03 16:21:44 +0300

    everything done !

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,28 +1,31 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 /**
- * get_op_func - function pointer
- * @s: operator passed as argument to the program
- * @Return: a pointer to the function that corresponds to the operator
- */
+ * get_op_func - Compares function with pointer and returns the equal.
+ * @s: points a char
+ * Return: Returns the function and the operator when equal
+**/
+
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
-	
-	while (ops[i].op != NULL)
-	{
-		if (!strcmp(ops[i].op, s))
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+
+int i = 0;
+
+while (ops[i].op != NULL)
+{
+if (!strcmp(ops[i].op, s))
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
