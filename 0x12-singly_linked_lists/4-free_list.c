490448cbbc209b5c6e48adfd4a48c7801841fcb2
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-11 16:21:41 +0300

    task 4 done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,47 @@
 #include "lists.h"
-
 /**
- * free_list - A function that frees all elements in a linked list
- * @head: pointer to head element of list
- * Return: Nothing
+ * _strlen - finding the lenght of a string.
+ * @s: first pointer
+ * Return: it returns the length of the string.
  */
-void free_list(list_t *head)
+int _strlen(char *s)
 {
-	list_t *temp;
+	int str;
 
-	temp = head;
-	while (head)
+	str = 0;
+	while (s[str] != '\0')
 	{
-		temp = head;
-		head = head->next;
-		free(temp->str);
-		free(temp);
+		str++;
 	}
-	free(head);
+	return (str);
+}
+/**
+ * add_node_end -  adds a new node at the end of a list_t list.
+ * @head: pointer to the addres of head
+ * @str: pointer to the string of each new node
+ * Return: addres to the new head node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *last;
+
+	if (!head)
+		return (0);
+	last = *head;/*last will have the value of head*/
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (0);
+	new->str = strdup(str);
+	new->len = _strlen((char *)str);
+	new->next = NULL;/*because next will be always null*/
+	if (*head == NULL)/*for the fisrt case*/
+	{
+		*head = new;
+		return (*head);
+	}
+	while (last->next)/*Unil it gest to the NULL*/
+		last = last->next;/*last will be the next of the last*/
+	last->next = new;/*in case next is null already*/
+	return (*head);
 }
