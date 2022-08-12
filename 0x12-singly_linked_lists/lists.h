6ff8bae2d2139db9cf43ea79a9dc365a4d2b876c
zeddymuge <cheroozeddy@gmail.com>
2022-08-12 01:50:21 -0700

    list.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-/*
- * File: lists.h
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
