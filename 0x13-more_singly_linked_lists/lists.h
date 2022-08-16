9304e168217a47814b25b1fa3f554227576a12e6
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-16 13:29:30 +0300

    everything done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
 /**
  * struct listint_s - singly linked list
- * @n: our data
+ * @n: integer
  * @next: points to the next node
  * Description singly linked list node structure
  */
+
 typedef struct listint_s
 {
 	int n;
