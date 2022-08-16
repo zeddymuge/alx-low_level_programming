86613e819391b48353180001c20f76721180ae97
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-16 14:45:09 +0300

    everything done

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,16 @@
 #include "lists.h"
-/**
- * listint_len -  function that returns the number of elements
- * in a linked listint_t list.
- * @h: contain elements
- * Return: number of elements
- */
+#include <stdio.h>
 
+/**
+ * listint_len - Returns the number of elements
+ *               in a linked listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the listint_t list.
+ */
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes;
+	size_t nodes = 0;
 
 	while (h)
 	{
